bea9e83460b8879b5f2b94234966697ac2fdf35c
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2014-11-12 21:50:53 +0100

    snakes!

--- source/bars.cpp
+++ source/bars.cpp
@@ -12,6 +12,8 @@ void drawBars()
     static unsigned int bufferw = 99999;
     static unsigned int bufferh = 99999;
     static unsigned char *buffer = 0;
+    static unsigned int time = 0;
+    time += 1;
 
     if(bufferw != w || bufferh != h)
     {
@@ -27,9 +29,36 @@ void drawBars()
         memset(buffer, 0, bufferw * bufferh);
     }
 
-    for(int i=0;i<bufferh;i++)
+    for(unsigned int y = 0; y < h; y++)
     {
-        buffer[i*bufferw+rand()%bufferw]=rand()%99;
+
+        for(unsigned int x = 0; x < w; x++)
+        {
+            buffer[x + y * bufferw] = 0;
+        }
+    }
+
+    for(unsigned int i = 0; i < bufferh; i++)
+    {
+        float s = sin((float)i / 5.0f + (float)time / 60.0f)*0.9;
+        s*=(float)i/(float)bufferh;
+        s*=bufferw/2;
+        s = s + bufferw / 2; // center
+        unsigned char c = ((time/10)+i) % 239;
+        unsigned char b = 50;
+        unsigned int pos = s;
+
+        for(unsigned int j = i; j < bufferh; j++)
+        {
+            buffer[j * bufferw + pos + 3] = colorGradient[b + 35][c];
+            buffer[j * bufferw + pos - 3] = colorGradient[b - 35][c];
+            buffer[j * bufferw + pos + 2] = colorGradient[b + 25][c];
+            buffer[j * bufferw + pos - 2] = colorGradient[b - 25][c];
+            buffer[j * bufferw + pos + 1] = colorGradient[b + 12][c];
+            buffer[j * bufferw + pos - 1] = colorGradient[b - 12][c];
+            buffer[j * bufferw + pos]     = colorGradient[b][c];
+
+        }
     }
 
     erase();
@@ -39,9 +68,13 @@ void drawBars()
 
         for(unsigned int x = 0; x < w; x++)
         {
-            unsigned char brightness = buffer[x + y * w];
-            attron(COLOR_PAIR(colorGradient[brightness][239]));
-            mvprintw(y, x, "x");
+            unsigned char color = buffer[x + y * w];
+
+            if(color != 0)
+            {
+                attron(COLOR_PAIR(color));
+                mvprintw(y, x, "x");
+            }
         }
     }
 
